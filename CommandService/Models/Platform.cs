60f83038bc3c3bb1af1f9ebf9b702ee90d0ae8df
Chris Claude <christ.tchambila@gmail.com>
2021-11-29 22:31:22 +0200

    updated command service

---
+++ CommandService/Models/Platform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int ExternalID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
+    }
+}
