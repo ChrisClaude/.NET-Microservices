ec491b10de0ec8bc5822092a998d9863b8415ca9
Chris Claude <christ.tchambila@gmail.com>
2021-12-14 20:21:15 +0200

    added grpc implementation

--- PlatformService/Profiles/PlatformProfile.cs
+++ PlatformService/Profiles/PlatformProfile.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace PlatformService.Profiles
 {
-    public class PlatformProfile : Profile 
+    public class PlatformProfile : Profile
     {
         public PlatformProfile()
         {
@@ -16,6 +16,10 @@ namespace PlatformService.Profiles
             CreateMap<Platform, PlatformReadDto>();
             CreateMap<PlatformCreateDto, Platform>();
             CreateMap<PlatformReadDto, PlatformPublishedDto>();
+            CreateMap<Platform, GrpcPlatformModel>()
+                .ForMember(dest => dest.PlatformId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Publisher, opt => opt.MapFrom(src => src.Publisher));
         }
     }
 }
